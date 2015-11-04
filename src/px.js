3da293da626f3f77bcaa6b5dde4ffd1968b94f0c
clementchi <clementchi@gmail.com>
2015-11-03 20:49:13 -0800

    change function name to slugify and add tests

--- src/px.js
+++ src/px.js
@@ -221,18 +221,10 @@
         return n === Number(n) && n % 1 !== 0;
     };
 
-    window.px.convertPropertyToAttributeName = function(propertyName){
-        // Separate camel-cased words with a space for later processing.
-        var attrName = propertyName.replace(/[A-Z]/g, function (s) {
-            return " " + s;
-        });
-        attrName = attrName.toLowerCase();
-        attrName = attrName.replace(/[^a-z0-9 -]/g, '') // remove invalid chars
-            .replace(/\s+/g, '-') // collapse whitespace and replace by -
-            .replace(/-+/g, '-'); // collapse dashes
-        // Trim leading and trailing whitespace and dashes.
-        attrName = attrName.replace(/^[\s|-]+|[\s|-]+$/g, '');
-        return attrName;
+    window.px.slugify = function(propName){
+        var noCapitals = propName.replace(/([a-z])([A-Z])/g, '$1 $2');
+        var allLowercase = noCapitals.toLowerCase();
+        var attributeName = allLowercase.replace(/\s/g, '-');
+        return attributeName;
     };
-
 })();
--- test/px-spec.js
+++ test/px-spec.js
@@ -158,4 +158,26 @@ describe('time series transformation', function() {
 
 });
 
+describe('polymer property to attribute test', function(){
+   it('can convert camelcase name to slug name', function(){
+      var attr = px.slugify('myProperty')
+      expect(attr).toBe('my-property');
+   });
+
+   it('when all lower case will not convert', function(){
+        var attr = px.slugify('myproperty')
+        expect(attr).toBe('myproperty');
+   });
+
+   it('when all upper case will convert to all lowercase', function(){
+        var attr = px.slugify('MYPROPERTY')
+        expect(attr).toBe('myproperty');
+   });
+
+   it('when all upper case will convert to all lowercase', function(){
+       var attr = px.slugify('MyProperty')
+       expect(attr).toBe('my-property');
+   });
+});
+
 
